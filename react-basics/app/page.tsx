786b1395938c60ce2c9f793034f2938313d73839
lmProgramming <mikikubs@gmail.com>
2025-04-15 16:30:24 +0200

    feat: react basics tutorial finished

--- react-basics/app/page.tsx
+++ react-basics/app/page.tsx
@@ -1,25 +1,23 @@
-"use client";
 import { useState } from "react";
+import LikeButton from "./like-button";
 
 function Header({ title }) {
   return <h1>Title {title ? title : "Default"}</h1>;
 }
+
 export default function HomePage() {
   const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"];
-  const [likes, setLikes] = useState(0);
-  function handleClick() {
-    setLikes(likes + 1);
-  }
+
   return (
     <div>
-      <header title="React" />
-      <header title="" />
+      <Header title="React" />
+      <Header title="" />
       <ul>
         {names.map((name) => (
           <li key={name}>{name}</li>
         ))}
       </ul>
-      <button onClick={handleClick}>Like ({likes})</button>
+      <LikeButton />
     </div>
   );
 }
